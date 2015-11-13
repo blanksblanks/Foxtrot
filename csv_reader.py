4b874fc9bb43f54c97dcbee41f4fab1dbf97dd3e
nb2406@columbia.edu <nb2406@columbia.edu>
2015-11-12 21:54:54 -0500

    Parse categories.csv into { item : (id, name) }

--- csv_reader.py
+++ csv_reader.py
@@ -1,6 +1,13 @@
 import csv
 
-with open('data/category.csv', 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        print row
+def parse_categories(filepath):
+    with open(filepath, 'rb') as csvfile:
+        categories = {}
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for item in row[2].split(', '):
+                categories[item.lower()] = (row[0].lower(), row[1].lower())
+        print categories
+        return categories
+
+parse_categories('data/category.csv')
