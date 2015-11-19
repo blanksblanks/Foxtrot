be4ab03a4c48c46403a39155f71b4800f52ac3dd
melissaKG <mhk2149@columbia.edu>
2015-11-19 16:28:51 -0500

    Changed the key to cetegory id instead of category name.

--- csv_reader.py
+++ csv_reader.py
@@ -1,12 +1,12 @@
 import csv
 
+# Read CSV file and make a dictionary for category names and descriptions.  
 with open('data/category.csv', 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
     categoryDict = dict()
     for row in reader:
-        categoryDict[row['Category Name']]=row['Category Description']
+        categoryDict[row['Category ID']]=row['Category Description']
     print categoryDict
-        #print(row['Category Name'])
 
 #def parse_categories(filepath):
 #    with open(filepath, 'rb') as csvfile:
