898de7168b3f848b333b9b42c6ec7401d991fd00
melissaKG <mhk2149@columbia.edu>
2015-11-27 19:17:10 -0500

    Added categories to the db.

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ db = client.foxtrot
 
 collection = db.foxtrot_collection
 
-# Insert document into the collection.
 from csv_reader import convertDict
 category = convertDict("data/category.csv", 'Category ID', 'Category Description')
+# Insert document into the collection.
 categories = db.categories
 category_id = categories.insert_one(category).inserted_id
 print category_id
+print db.collection_names(include_system_collections=False)
+print categories.find_one()
